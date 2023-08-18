6ea6a5d789de41ddce0a5a25b2b289ec842391d8
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-08-18 10:37:12 +0300

    REMAKE

--- App.js
+++ App.js
@@ -1,65 +1,33 @@
+import React, { useEffect } from "react";
+
 import { useFonts } from "expo-font";
+
+import RootNavigator from "./src/navigation/RootNavigator";
 import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
-import { LoginScreen } from "./screens/LoginScreen";
-import { RegistrationScreen } from "./screens/RegistrationScreen";
-import { Home } from "./screens/Home";
-import { CommentsScreen } from "./screens/CommentsScreen";
-
-import { BackButtonComponent } from "./components/BackButtonComponent";
+import * as Font from "expo-font";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
   });
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+        "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+        "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+      });
+    }
+    loadFonts();
+  }, []);
 
   if (!fontsLoaded) {
     return null;
   }
 
-  const MainStack = createStackNavigator();
-
-  return (
-    <NavigationContainer>
-      <MainStack.Navigator initialRouteName="Login">
-        <MainStack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <MainStack.Screen
-          name="Registration"
-          component={RegistrationScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <MainStack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <MainStack.Screen
-          name="Comments"
-          component={CommentsScreen}
-          options={{
-            title: "Коментарі",
-            headerTitleAlign: "center",
-            headerShown: true,
-            headerLeft: () => <BackButtonComponent />,
-            headerStyle: {
-              borderBottomWidth: 1,
-            },
-          }}
-        />
-      </MainStack.Navigator>
-    </NavigationContainer>
-  );
+  return <RootNavigator />;
 }
